0462895a231a0c83c1912bbedc90559c4853ba0b
PythonFarzanNezarat <pythonfarzannezarat@gmail.com>
2022-09-03 21:45:48 +0430

    write inche command

--- Commands.py
+++ Commands.py
@@ -18,10 +18,11 @@ class Commands:
             result = self.entryFrom / 914.4
         elif self.listBoxTo == 'Meter':
             result = (self.entryFrom / 10) / 100
-        elif self.listBoxTo == 'Mile':
-            result = self.entryFrom / 1609344
         elif self.listBoxTo == 'KiloMeter':
             result = ((self.entryFrom / 10)/100)/1000
+        elif self.listBoxTo == 'Mile':
+            result = self.entryFrom / 1609344
+
         return result
 
     def __centimeter(self):
@@ -38,10 +39,32 @@ class Commands:
             result = self.entryFrom / 91.44
         elif self.listBoxTo == 'Meter':
             result = self.entryFrom / 100
-        elif self.listBoxTo == 'Mile':
-            result = self.entryFrom / 160934.4
         elif self.listBoxTo == 'KiloMeter':
             result = self.entryFrom/100000
+        elif self.listBoxTo == 'Mile':
+            result = self.entryFrom / 160934.4
+
+        return result
+
+    def __inch(self):
+        result = 0
+        if self.listBoxTo == 'MilliMeter':
+            result = self.entryFrom * 25.4
+        elif self.listBoxTo == 'CentiMeter':
+            result = self.entryFrom * 2.54
+        elif self.listBoxTo == 'Inch':
+            result = self.entryFrom
+        elif self.listBoxTo == 'Feet':
+            result = self.entryFrom / 12
+        elif self.listBoxTo == 'Yard':
+            result = self.entryFrom / 36
+        elif self.listBoxTo == 'Meter':
+            result = self.entryFrom / 39.3700787402
+        elif self.listBoxTo == 'KiloMeter':
+            result = self.entryFrom / 39370.078740157
+        elif self.listBoxTo == 'Mile':
+            result = self.entryFrom / 63360
+
         return result
 
     def units(self):
@@ -49,3 +72,5 @@ class Commands:
             return self.__millimeter()
         elif self.listBoxFrom == 'CentiMeter':
             return self.__centimeter()
+        elif self.listBoxFrom == 'Inch':
+            return self.__inch()
